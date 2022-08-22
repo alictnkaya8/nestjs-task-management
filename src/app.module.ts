7dc8479ca8b76f2f0f0aa11291cede11b1ddab8c
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-08-22 16:27:01 +0300

    add test for tasksService.getTasks and getTaskById

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from './tasks/task.entity';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
-
 
 @Module({
   imports: [
