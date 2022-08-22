8fa996da49448b7899362e6391129a494e52a5e9
alictnkaya8 <49043825+alictnkaya8@users.noreply.github.com>
2022-08-22 11:24:34 +0300

    add jwt and sign in method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,8 +7,13 @@ export class AuthController {
 
     constructor(private authService: AuthService) {}
 
-    @Post('signUp')
+    @Post('signup')
     signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signUp(authCredentialsDto);
+    }
+
+    @Post('signin')
+    signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
+        return this.authService.signIn(authCredentialsDto);
     }
 }
